b33f13a686cd48caf964cdbddfd58ef750a676eb
syv <cactus30301@gmail.com>
2026-01-07 23:17:12 +0900

    연습: 07 함수 선언 호출 반환

--- 자바스크립트/01-변수-데이터-타입/07-함수-선언-호출-반환/연습-시작.js
+++ 자바스크립트/01-변수-데이터-타입/07-함수-선언-호출-반환/연습-시작.js
@@ -8,11 +8,15 @@ const CURRENT_YEAR = 2026
 // 숫자 하나를 입력받아 그 수의 제곱(n * n)을 반환하는 함수를 만드세요.
 
 // 1. 함수 선언 (함수명: square)
+function square(number) {
+return (number) ** 2
+}
 
 
 // 2. 함수 호출 및 결과 확인 (입력값: 12)
 // 출력 결과: 144 (number)
-let resultSquare
+let resultSquare = square(12)
+console.log (resultSquare, typeof resultSquare)
 
 
 // --------------------------------------------------------------------------
@@ -22,10 +26,13 @@ let resultSquare
 
 // 1. 함수 선언 (함수명: introduce)
 // 매개변수: name, birthYear
-
+function introduce(name, birthYear) {
+  return `제 이름은 ${name}이고, ${CURRENT_YEAR - birthYear}살 이에요`
+}
 
 // 출력 결과 (예시): "제 이름은 박한영이고, 올해 26살입니다."
-let resultIntro
+let resultIntro = introduce('킹프린세스', 1950)
+console.log (resultIntro)
 
 
 // --------------------------------------------------------------------------
@@ -38,12 +45,21 @@ let resultIntro
 // 함수 표현식으로 작성 (변수명: canBuy)
 // 매개변수: budget, price, quantity
 
+function canBuy(budget, price, quantity) {
+budget >= (price * quantity) 
+return budget >= (price * quantity)
+} 
+console.log(canBuy(5000,2000,3))
+
+
 // 구매 가능한 경우 테스트
 // 가진 돈: 10,000원, 가격: 3,000원, 개수: 3개
 // 출력 결과: true (boolean)
-let checkSuccess
+let checkSuccess = canBuy(10000, 3000, 3)
+console.log(!!checkSuccess, typeof checkSuccess)
 
 // 구매 불가능한 경우 테스트
 // 가진 돈: 5,000원, 가격: 2,000원, 개수: 3개
 // 출력 결과: false (boolean)
-let checkFail
+let checkFail = canBuy(5000, 2000, 3)
+console.log(!!checkFail, typeof checkFail)
